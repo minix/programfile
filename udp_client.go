f92251ea4d303092610cb6e6a3b86c70e5a0692e
mgtuiw <mgtuiw@gmail.com>
2017-09-11 23:25:13 +0800

    change udp_client file

--- udp_client.go
+++ udp_client.go
@@ -1,39 +1,26 @@
 package main
 
 import (
-	"fmt"
+	//	"fmt"
+	"io/ioutil"
 	"net"
+	"os"
 )
 
 func main() {
-	socket, err := net.DialUDP("udp4", nil, &net.UDPAddr{
-		IP:   net.IPv4(127, 0, 0, 1),
-		Port: 23456,
-	})
-
-	if err != nil {
-		fmt.Println("connect Faild", err)
-		return
-	}
+	socket, err := net.DialUDP("udp", nil, &net.UDPAddr{IP: net.IPv4(127, 0, 0, 1), Port: 23456})
+	checkErr(err)
 
 	defer socket.Close()
 
-	senddata := []byte("hello Minix")
+	data, err := ioutil.ReadAll(os.Stdin)
 
-	_, err = socket.Write(senddata)
+	_, err = socket.Write(data)
+	checkErr(err)
+}
 
+func checkErr(err error) {
 	if err != nil {
-		fmt.Println("send data Faild!", err)
-		return
+		panic(err)
 	}
-
-	data := make([]byte, 4096)
-	read, remoteAddr, err := socket.ReadFromUDP(data)
-	if err != nil {
-		fmt.Println("read data Faild!", err)
-		return
-	}
-
-	fmt.Println(read, remoteAddr)
-	fmt.Printf("%s\n", data)
 }
